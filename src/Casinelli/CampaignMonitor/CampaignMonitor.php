3cef032b8a31991f915ce9b65e0be452ab2d965e
Giovanni Casinelli <giovanni.casinelli@gmail.com>
2015-10-12 22:04:42 +0200

    Add some basic wrapper logic

--- src/Casinelli/CampaignMonitor/CampaignMonitor.php
+++ src/Casinelli/CampaignMonitor/CampaignMonitor.php
@@ -1,11 +1,36 @@
 <?php
 
+namespace Casinelli\CampaignMonitor;
+
 class CampaignMonitor
 {
     protected $app;
 
+    protected $csRestSubscibers = null;
+
     public function __construct($app)
     {
         $this->app = $app;
+    }
+
+    public function subscribers($listId)
+    {
+        return $this->getRESTSubscribers($listId);
+    }
+
+    protected function getCsRestSubscribers($listId)
+    {
+        if ($this->csRestSubscribers) {
+            return $this->csRestSubscibers;
+        }
+
+        return $this->csRestSubscribers = new CS_REST_Subscribers($listId, $this->getAuthTokens());
+    }
+
+    protected function getAuthTokens()
+    {
+        return [
+            'access_token' => $this->app['config']['campaignmonitor.api_key'],
+        ];
     }
 }
